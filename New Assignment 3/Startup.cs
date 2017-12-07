9241af89ffb27a3e89b2cede0afddd4397e3cc87
Min Gi <Min Gi@LAPTOP-FPSD9HOC>
2017-12-07 11:53:13 -0500

    Add project files.

---
+++ New Assignment 3/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(New_Assignment_3.Startup))]
+namespace New_Assignment_3
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
